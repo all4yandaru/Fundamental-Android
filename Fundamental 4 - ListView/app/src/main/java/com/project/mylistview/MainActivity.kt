54f0f8804e6049cd5f3df13ae40c83a5c9397cfd
Liek Allyandaru <all4yandaru@gmail.com>
2020-10-18 17:26:57 +0700

    ListView

---
+++ Fundamental 4 - ListView/app/src/main/java/com/project/mylistview/MainActivity.kt
@@ -0,0 +1,64 @@
+package com.project.mylistview
+
+import android.content.res.TypedArray
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    // TODO 2: buat findview, adapter array dan array nama pahlawan
+    //private val dataName = arrayOf("Cut Nyak Dien", "Ki Hajar Dewantara", "Moh Yamin", "Patimura", "R A Kartini", "Sukarno")
+
+    // TODO 10: buat adapter dan hubungin
+    private lateinit var adapter: HeroAdapter
+
+    // TODO 11: tambahin ini untuk nampung masing2 data nama, desc, dan foto
+    private lateinit var dataName: Array<String>
+    private lateinit var dataDescription: Array<String>
+    private lateinit var dataPhoto: TypedArray
+    private var heroes = arrayListOf<Hero>()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val listView : ListView = findViewById(R.id.lv_list)
+        // val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, dataName)
+        adapter = HeroAdapter(this)
+        listView.adapter = adapter
+
+        // TODO 12: buat method prepare utk masukin data ke variabel yg udah dibuat
+        prepare()
+
+        // TODO 13: buat addItem utk masukin data tsb ke model
+        addItem()
+
+
+        // TODO 14: tambahin onItemClickListener buat pemanis
+        listView.onItemClickListener = AdapterView.OnItemClickListener { _, _, position, _ ->
+            Toast.makeText(this@MainActivity, heroes[position].name, Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun prepare(){
+        dataName = resources.getStringArray(R.array.data_name)
+        dataDescription = resources.getStringArray(R.array.data_description)
+        dataPhoto = resources.obtainTypedArray(R.array.data_photo)
+    }
+
+    private fun addItem(){
+        for (position in dataName.indices) {
+            val hero = Hero(
+                dataPhoto.getResourceId(position, -1),
+                dataName[position],
+                dataDescription[position]
+            )
+            heroes.add(hero)
+        }
+        adapter.heroes = heroes
+    }
+}
